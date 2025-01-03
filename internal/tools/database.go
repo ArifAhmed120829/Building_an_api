c4ef807edbe40f743d2ad6cd33c2f05390520942
ArifAhmed <arif15-12301@diu.edu.bd>
2025-01-03 12:27:10 +0600

    finished

---
+++ internal/tools/database.go
@@ -0,0 +1,28 @@
+package tools
+import (
+	log "github.com/sirupsen/logrus"
+)
+type LoginDetails struct{
+	AuthToken string
+	Username string
+}
+type CoinDetaills struct{
+	Coins int64
+	Username string
+}
+
+type DatabaseInterface interface{
+	GetUserLoginDetails(username string) *LoginDetails
+	GetUserCoins(username string) *CoinDetails
+	SetupDatabase() error
+}
+
+func NewDatabase() (*DatabaseInterface, error) {
+	var database DatabaseInterface = &mockDB{}
+	var err error = database.SetupDatabase()
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	return &database, nil
+}
